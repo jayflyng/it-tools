fb0d272d14f670eff574e3a219cfd8b14d53028b
jayflyng <1677016493@qq.com>
2025-12-31 10:19:18 +0800

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,9 @@
 export default {
   async fetch(request, env) {
-    // 直接返回编译后的Vue静态资源
-    return env.ASSETS.fetch(request);
-  }
+    // 优先返回静态资源
+    const asset = await env.ASSETS.fetch(request);
+    if (asset.status !== 404) return asset;
+    // 自定义Worker逻辑
+    return new Response("Vue + Workers 结合部署成功！");
+  },
 };
